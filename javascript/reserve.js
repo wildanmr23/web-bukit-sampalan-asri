e754b9fce49906a96ab98a737cb4b1cb9a3d5cec
wildanmr23 <wildanmursalin.28@gmail.com>
2024-09-04 06:34:14 +0700

    maintenance

---
+++ javascript/reserve.js
@@ -0,0 +1,16 @@
+function submitReservation() {
+  var name = document.getElementById("name").value;
+  var phone = document.getElementById("phone").value;
+  var date = document.getElementById("date").value;
+  var activity = document.getElementById("activity").value;
+  var notes = document.getElementById("notes").value;
+
+  var message = `Halo, saya ${name} ingin melakukan reservasi untuk ${activity} pada tanggal ${date}. No. telepon: ${phone}. Catatan tambahan: ${notes}`;
+
+  // Encode message to URL format
+  var whatsappURL =
+    "https://wa.me/+6282127312767?text=" + encodeURIComponent(message);
+
+  // Open WhatsApp chat
+  window.open(whatsappURL, "_blank");
+}
